efa3fde3f2fbb94fc1628b2bd4f21326e77e9084
son-sar <sonja.sartys@endocode.com>
2022-10-18 15:13:51 +0200

    code for app js prettier

--- websearch_node/views/app.js
+++ websearch_node/views/app.js
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Start the server
-
 const express = require('express');
 const path = require('path');
 
